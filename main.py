85d5fa7ef19c577eb3458d9f65c0fe2ae6f10f89
Giovani Santana <giovanisantanaa7@gmail.com>
2026-06-13 09:40:16 -0300

    adicionado puzzles 10x10

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     from agente_regras import AgenteRegras
     from agente_csp import AgenteCSP
     from agente_probabilistico import AgenteProbabilistico
-    from puzzles import puzzles_5x5
+    from puzzles import puzzles_5x5, puzzles_10x10
 
     agentes = [AgenteRegras(), AgenteCSP(), AgenteProbabilistico()]
 
-    for puzzle in puzzles_5x5():
+    for puzzle in puzzles_5x5() + puzzles_10x10():
         print("=== " + puzzle.nome + " ===")
         for agente in agentes:
             p = puzzle_original = puzzle.copiar()
